7c9cfa351575ee7f143bc5294b612937332c589f
dawsonrichey <dawsonrichey89@gmail.com>
2021-10-05 04:41:35 -0400

    1 file modified, 5 added

---
+++ new.js
@@ -0,0 +1,12 @@
+const arr = [
+  { name: 'Sarene', role: 'Help Desk Operator', country: 'Thailand' },
+  { name: 'Olvan', role: 'Nurse Practicioner', country: 'China' },
+  { name: 'Janos', role: 'Cost Accountant', country: 'China' },
+  { name: 'Dolph', role: 'Assistant Manager', country: 'China' },
+  { name: 'Ariela', role: 'Database Administrator I', country: 'Azerbaijan' },
+  { name: 'Lane', role: 'Environmental Tech', country: 'Indonesia' },
+  { name: 'Griselda', role: 'Senior Quality Engineer', country: 'Portugal' },
+  { name: 'Manda', role: 'Physical Therapy Assistant', country: 'Brazil' },
+  { name: 'Leslie', role: 'Information Systems Manager', country: 'Japan' },
+  { name: 'Aleen', role: 'Cost Accountant', country: 'Canada' },
+];
